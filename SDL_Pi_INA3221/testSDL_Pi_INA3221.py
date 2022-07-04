a4a5aa39733c4bce8f370c171fca4cba08d2ac5c
marvin-steinke <marvin.steinke@mail.de>
2022-07-04 11:45:58 +0200

    ported code from python 2 to 3

--- SDL_Pi_INA3221/testSDL_Pi_INA3221.py
+++ SDL_Pi_INA3221/testSDL_Pi_INA3221.py
@@ -11,18 +11,18 @@
 import sys
 import time
 import datetime
-import random 
+import random
 import SDL_Pi_INA3221
 
 # Main Program
 
-print ""
-print "Test SDL_Pi_INA3221 Version 1.0 - SwitchDoc Labs"
-print ""
-print "Sample uses 0x40 and SunAirPlus board INA3221"
-print " Will work with the INA3221 SwitchDoc Labs Breakout Board"
-print "Program Started at:"+ time.strftime("%Y-%m-%d %H:%M:%S")
-print ""
+print("")
+print("Test SDL_Pi_INA3221 Version 1.0 - SwitchDoc Labs")
+print("")
+print("Sample uses 0x40 and SunAirPlus board INA3221")
+print(" Will work with the INA3221 SwitchDoc Labs Breakout Board")
+print("Program Started at:"+ time.strftime("%Y-%m-%d %H:%M:%S"))
+print("")
 
 filename = time.strftime("%Y-%m-%d%H:%M:%SRTCTest") + ".txt"
 starttime = datetime.datetime.utcnow()
@@ -37,58 +37,58 @@ OUTPUT_CHANNEL       = 3
 
 while True:
 
-  	print "------------------------------"
-  	shuntvoltage1 = 0
-  	busvoltage1   = 0
-  	current_mA1   = 0
-  	loadvoltage1  = 0
+    print("------------------------------")
+    shuntvoltage1 = 0
+    busvoltage1   = 0
+    current_mA1   = 0
+    loadvoltage1  = 0
 
 
-  	busvoltage1 = ina3221.getBusVoltage_V(LIPO_BATTERY_CHANNEL)
-  	shuntvoltage1 = ina3221.getShuntVoltage_mV(LIPO_BATTERY_CHANNEL)
-  	# minus is to get the "sense" right.   - means the battery is charging, + that it is discharging
-  	current_mA1 = ina3221.getCurrent_mA(LIPO_BATTERY_CHANNEL)  
+    busvoltage1 = ina3221.getBusVoltage_V(LIPO_BATTERY_CHANNEL)
+    shuntvoltage1 = ina3221.getShuntVoltage_mV(LIPO_BATTERY_CHANNEL)
+    # minus is to get the "sense" right.   - means the battery is charging, + that it is discharging
+    current_mA1 = ina3221.getCurrent_mA(LIPO_BATTERY_CHANNEL)
 
-  	loadvoltage1 = busvoltage1 + (shuntvoltage1 / 1000)
-  
-  	print "LIPO_Battery Bus Voltage: %3.2f V " % busvoltage1
-  	print "LIPO_Battery Shunt Voltage: %3.2f mV " % shuntvoltage1
-  	print "LIPO_Battery Load Voltage:  %3.2f V" % loadvoltage1
-  	print "LIPO_Battery Current 1:  %3.2f mA" % current_mA1
-  	print
+    loadvoltage1 = busvoltage1 + (shuntvoltage1 / 1000)
 
-  	shuntvoltage2 = 0
-  	busvoltage2 = 0
-  	current_mA2 = 0
-  	loadvoltage2 = 0
+    print("LIPO_Battery Bus Voltage: %3.2f V " % busvoltage1)
+    print("LIPO_Battery Shunt Voltage: %3.2f mV " % shuntvoltage1)
+    print("LIPO_Battery Load Voltage:  %3.2f V" % loadvoltage1)
+    print("LIPO_Battery Current 1:  %3.2f mA" % current_mA1)
+    print()
 
-  	busvoltage2 = ina3221.getBusVoltage_V(SOLAR_CELL_CHANNEL)
-  	shuntvoltage2 = ina3221.getShuntVoltage_mV(SOLAR_CELL_CHANNEL)
-  	current_mA2 = -ina3221.getCurrent_mA(SOLAR_CELL_CHANNEL)
-  	loadvoltage2 = busvoltage2 + (shuntvoltage2 / 1000)
-  
-  	print "Solar Cell Bus Voltage 2:  %3.2f V " % busvoltage2
-  	print "Solar Cell Shunt Voltage 2: %3.2f mV " % shuntvoltage2
-  	print "Solar Cell Load Voltage 2:  %3.2f V" % loadvoltage2
-  	print "Solar Cell Current 2:  %3.2f mA" % current_mA2
-  	print 
+    shuntvoltage2 = 0
+    busvoltage2 = 0
+    current_mA2 = 0
+    loadvoltage2 = 0
 
-  	shuntvoltage3 = 0
-  	busvoltage3 = 0
-  	current_mA3 = 0
-  	loadvoltage3 = 0
+    busvoltage2 = ina3221.getBusVoltage_V(SOLAR_CELL_CHANNEL)
+    shuntvoltage2 = ina3221.getShuntVoltage_mV(SOLAR_CELL_CHANNEL)
+    current_mA2 = -ina3221.getCurrent_mA(SOLAR_CELL_CHANNEL)
+    loadvoltage2 = busvoltage2 + (shuntvoltage2 / 1000)
 
-  	busvoltage3 = ina3221.getBusVoltage_V(OUTPUT_CHANNEL)
-  	shuntvoltage3 = ina3221.getShuntVoltage_mV(OUTPUT_CHANNEL)
-  	current_mA3 = ina3221.getCurrent_mA(OUTPUT_CHANNEL)
-  	loadvoltage3 = busvoltage3 + (shuntvoltage3 / 1000)
-  
-  	print "Output Bus Voltage 3:  %3.2f V " % busvoltage3
-  	print "Output Shunt Voltage 3: %3.2f mV " % shuntvoltage3
-  	print "Output Load Voltage 3:  %3.2f V" % loadvoltage3
-  	print "Output Current 3:  %3.2f mA" % current_mA3
-  	print
-		
+    print("Solar Cell Bus Voltage 2:  %3.2f V " % busvoltage2)
+    print("Solar Cell Shunt Voltage 2: %3.2f mV " % shuntvoltage2)
+    print("Solar Cell Load Voltage 2:  %3.2f V" % loadvoltage2)
+    print("Solar Cell Current 2:  %3.2f mA" % current_mA2)
+    print()
 
-	#
-	time.sleep(2.0)
+    shuntvoltage3 = 0
+    busvoltage3 = 0
+    current_mA3 = 0
+    loadvoltage3 = 0
+
+    busvoltage3 = ina3221.getBusVoltage_V(OUTPUT_CHANNEL)
+    shuntvoltage3 = ina3221.getShuntVoltage_mV(OUTPUT_CHANNEL)
+    current_mA3 = ina3221.getCurrent_mA(OUTPUT_CHANNEL)
+    loadvoltage3 = busvoltage3 + (shuntvoltage3 / 1000)
+
+    print("Output Bus Voltage 3:  %3.2f V " % busvoltage3)
+    print("Output Shunt Voltage 3: %3.2f mV " % shuntvoltage3)
+    print("Output Load Voltage 3:  %3.2f V" % loadvoltage3)
+    print("Output Current 3:  %3.2f mA" % current_mA3)
+    print()
+
+
+    #
+    time.sleep(2.0)
--- testWatchDog.py
+++ testWatchDog.py
@@ -10,11 +10,9 @@
 # Standard Grove to Grove Cable from Grove WatchDog Plub on SunControl to D13/D16 on Pi2Grover Board
 # Female Pin Header to Grove Cable Adaptor Cable (for WatchDog Out Pulse High Pin on SunControl Board)
 
-
 # imports
 
 import sys
-
 
 import time
 import datetime
@@ -24,22 +22,19 @@ import SDL_Pi_SunControl
 
 # Main Program
 
-print ""
-print "Test SDL_Pi_SunControl WatchDog Version 1.0 - SwitchDoc Labs"
-print ""
-print "Program Started at:"+ time.strftime("%Y-%m-%d %H:%M:%S")
-print ""
+print("")
+print("Test SDL_Pi_SunControl WatchDog Version 1.0 - SwitchDoc Labs")
+print("")
+print("Program Started at:"+ time.strftime("%Y-%m-%d %H:%M:%S"))
+print("")
 
 # configuration
-
 
 # Grove connector from Grove WatchDog SunControl Board to Raspberry Pi (D13/D16 on Pi2Grover Board)
 Pin_WatchDog_Done = 13
 Pin_WatchDog_Wake = 16
 
 Pin_WatchDog_Reset = 4
-
-
 
 sunControl = SDL_Pi_SunControl.SDL_Pi_SunControl(WatchDog_Done=Pin_WatchDog_Done,  WatchDog_Wake=Pin_WatchDog_Wake)
 
@@ -53,34 +48,27 @@ Mode = 1
 
 # Set up Two Interrupts on Wake and ResetPulseHigh (ResetN Open Drain pulses Low
 def Wake_callback(pin):
-
-	print "Pin=%i Wake Rising Pulse Detected: %s"% (pin, datetime.datetime.now().strftime("%H:%M:%S"))
+    print("Pin=%i Wake Rising Pulse Detected: %s"% (pin, datetime.datetime.now().strftime("%H:%M:%S")))
 
 
 def WatchDog_Reset_callback(pin):
-
-	print "Pin=%i WatchDog Reset  Rising Pulse Detected: %s"% (pin, datetime.datetime.now().strftime("%H:%M:%S"))
-
-
+    print("Pin=%i WatchDog Reset  Rising Pulse Detected: %s"% (pin, datetime.datetime.now().strftime("%H:%M:%S")))
 
 GPIO.setup(Pin_WatchDog_Reset, GPIO.IN)
 
-
-GPIO.add_event_detect(Pin_WatchDog_Wake, GPIO.RISING, callback=Wake_callback )  
-GPIO.add_event_detect(Pin_WatchDog_Reset, GPIO.RISING, callback=WatchDog_Reset_callback )  
+GPIO.add_event_detect(Pin_WatchDog_Wake, GPIO.RISING, callback=Wake_callback )
+GPIO.add_event_detect(Pin_WatchDog_Reset, GPIO.RISING, callback=WatchDog_Reset_callback )
 
 while True:
 
-	# Free Running - Done every 64 seconds (default) and NO WAKE signal
-	if (Mode == 0):
-		print "--->"+datetime.datetime.now().strftime("%H:%M:%S")
-		time.sleep(10.0);
-	
-	# Pat the Dog every 60 seconds
-	if (Mode == 1):
-		print "--->"+datetime.datetime.now().strftime("%H:%M:%S")
-		sunControl.patTheWatchDog()
-		print "WatchDog Patted"
-		time.sleep(60);
-	
+    # Free Running - Done every 64 seconds (default) and NO WAKE signal
+    if (Mode == 0):
+        print("--->"+datetime.datetime.now().strftime("%H:%M:%S"))
+        time.sleep(10.0);
 
+    # Pat the Dog every 60 seconds
+    if (Mode == 1):
+        print("--->"+datetime.datetime.now().strftime("%H:%M:%S"))
+        sunControl.patTheWatchDog()
+        print("WatchDog Patted")
+        time.sleep(60);
